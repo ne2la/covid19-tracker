fb461727dfc0659305035d5ce21b3dca471b7ca3
ne2la <madushan.20191085@iit.ac.lk>
2021-08-25 20:08:21 +0530

    add header and footer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/pages/Header';
+import Footer from './components/pages/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h3> COVID-19 TRACKER </h3>
+        </div>
+    )
+}
+
+export default Header
